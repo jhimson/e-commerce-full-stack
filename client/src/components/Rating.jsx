a9a8c29114e13f42fc1e262b5406a76cb95d83b4
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-09-05 21:25:32 -0400

    feat: add rating component to each products

---
+++ client/src/components/Rating.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable no-nested-ternary */
+import React from 'react';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
+
+const Rating = ({ value, text }) => (
+  <div className="flex items-center space-x-1 rating">
+    <div className="flex">
+      <span>
+        {value >= 1 ? (
+          <BsStarFill className="text-yellow-400" />
+        ) : value >= 0.5 ? (
+          <BsStarHalf className="text-yellow-400" />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+      <span>
+        {value >= 2 ? (
+          <BsStarFill className="text-yellow-400" />
+        ) : value >= 1.5 ? (
+          <BsStarHalf className="text-yellow-400" />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+      <span>
+        {value >= 3 ? (
+          <BsStarFill className="text-yellow-400" />
+        ) : value >= 2.5 ? (
+          <BsStarHalf className="text-yellow-400" />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+      <span>
+        {value >= 4 ? (
+          <BsStarFill className="text-yellow-400" />
+        ) : value >= 3.5 ? (
+          <BsStarHalf className="text-yellow-400" />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+      <span>
+        {value >= 5 ? (
+          <BsStarFill className="text-yellow-400" />
+        ) : value >= 4.5 ? (
+          <BsStarHalf className="text-yellow-400" />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    </div>
+    <h5 className="text-lg">{text && text}</h5>
+  </div>
+);
+
+export default Rating;

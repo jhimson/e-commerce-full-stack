bcdab9e8cf97962a593e6eb454343c1a53a8ff02
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-09-05 21:56:04 -0400

    feat: add product route

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,13 @@ import RegisterPage from './pages/RegisterPage';
 import PageNotFound from './pages/PageNotFound';
 import DashboardPage from './pages/DashboardPage';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={HomeScreen} exact />
+      <Route path="/product/:id" component={ProductScreen} />
       <Route path="/register" component={RegisterPage} exact />
       <Route path="/dashboard" component={DashboardPage} exact />
       <Route component={PageNotFound} exact />

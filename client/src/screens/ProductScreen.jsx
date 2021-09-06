32d1b7ca99747e25645e21391dce946f6a894aa5
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-09-05 21:57:00 -0400

    feat: add ProductScreen page

---
+++ client/src/screens/ProductScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const ProductScreen = () => {
+  const x = 0;
+  return (
+    <Layout>
+      <div className="h-screen">
+        <h1>Product Screen Test</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default ProductScreen;

b7f6dbc1d6e77b62c2c7908a65fec6ecc977787f
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-09-19 17:35:51 -0400

    fix: format of error handler middleware

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -5,8 +5,9 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error.message, error.status);
-  res.status(error.status || 500);
+  // console.log(error.message, error.status);
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
   res.json({
     error: { message: error.message, status: error.status },
     stack: process.env.NODE_ENV !== "production" ? null : error.stack,

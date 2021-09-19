ac4db0af760e9a1fde75567c29ea2630ef11629a
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-09-19 19:38:58 -0400

    feat: add productListReducer to the root reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,11 +2,14 @@ import { combineReducers } from 'redux';
 
 //! REDUCERS
 import { userRegisterReducer, userLoginReducer } from './userReducer';
+
+import { productListReducer } from './productReducer';
 //!
 
 const rootReducer = combineReducers({
   userRegister: userRegisterReducer,
   userLogin: userLoginReducer,
+  productList: productListReducer,
 });
 
 export default rootReducer;

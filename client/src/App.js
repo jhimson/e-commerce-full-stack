3fee2a49f3fd75e72bc2d019a9f348401ce8c1be
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-10-02 14:44:35 -0400

    add cart screen to react router

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,14 @@ import PageNotFound from './pages/PageNotFound';
 import DashboardPage from './pages/DashboardPage';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={HomeScreen} exact />
       <Route path="/product/:id" component={ProductScreen} />
+      <Route path="/cart/:id?" component={CartScreen} />
       <Route path="/login" component={LoginPage} exact />
       <Route path="/register" component={RegisterPage} exact />
       <Route path="/dashboard" component={DashboardPage} exact />
